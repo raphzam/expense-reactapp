754464f5cef45ae2364721ce5f4e7422508726ca
raphzam <60793109+raphzam@users.noreply.github.com>
2021-08-05 18:26:26 -0400

    Create category controller with CRUD operations

---
+++ src/main/java/com/example/expense/controller/CategoryController.java
@@ -0,0 +1,60 @@
+package com.example.expense.controller;
+
+import com.example.expense.model.Category;
+import com.example.expense.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/categories")
+    public Collection<Category> getCategories() {
+        return categoryRepository.findAll();
+        //select * from category
+    }
+
+    //category/2
+    @GetMapping("/category/{id}")
+    ResponseEntity<?> getCategory(@PathVariable Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/category")
+    ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        Category result = categoryRepository.save(category);
+        //insert into table....
+
+        return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
+    }
+
+    @PutMapping("/category/{id}")
+    ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/category/{id}")
+    ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        categoryRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
