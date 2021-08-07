9e35afe22373a6350b7aa90d27f453b816d47a6f
raphzam <60793109+raphzam@users.noreply.github.com>
2021-08-07 17:04:56 -0400

    Build nav and pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Category from "./Category";
+import Home from "./Home";
+import Expenses from "./Expenses";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/categories" exact={true} component={Category} />
+          <Route path="/expenses" exact={true} component={Expenses} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
